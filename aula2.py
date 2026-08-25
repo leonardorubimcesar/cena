ac04c3488a0dfda9dd581d14116e23908a8690a3
leonardorubimcesar-byte <leonardorubimcesar@gmail.com>
2026-08-25 19:05:38 -0300

    continuação da aula

--- aula2.py
+++ aula2.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
-nome = "Leonardo"
-print (nome)
+#nome = "Leonardo"
+#print('Meu nome: ', nome)
+#cor = "Roxo"
+#print('Minha cor favorita: ', cor)
+#atividade = "Ler"
+#print('Minha atividade favorita: ', atividade)
+#animal = "Gato"
+#print('Meu animal favorito: ', animal)
+import sys
+argumento1 = sys.argv[1]
+argumento2 = sys.argv[2]
+argumento3 = sys.argv[3]
+argumento4 = sys.argv[4]
+print('Meu nome: '+ argumento1, "\nMinha cor favorita: " + argumento2, "\nMinha atividade favorita: " + argumento3, "\nMeu animal favorito: " + argumento4)
+
+
