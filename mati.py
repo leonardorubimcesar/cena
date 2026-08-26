b33bac88e8aec1b14c19cb37079bb53dbe385e90
leonardorubimcesar-byte <leonardorubimcesar@gmail.com>
2026-08-26 10:30:47 -0300

    versão 4 script aula 2

--- mati.py
+++ mati.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
-numero = 49
+numero = 51
 if numero < 0:
     message = 'negativo'
     print(message)
-elif numero > 0 and numero < 50:
-    message = 'positivo e menor que 50'
-    print(message)
-elif numero > 0 and numero > 50:
-    message = 'positivo e maior que 50'
-    print(message)
+elif numero > 0 and numero < 50 and numero % 2 == 0:
+    message = 'é um número par menor que 50'
+    print(número, message)
+elif numero > 0 and numero < 50 and numero % 2 != 0:
+    message = 'é um número ímpar menor que 50'
+    print(numero, message)
+elif numero > 0 and numero > 50 and numero % 3 == 0:
+    message = 'é maior que 50 e divisível por 3'
+    print(numero, message)
+elif numero > 50 and numero % 3 != 0:
+    message = 'é maior que 50 e não divisível por 3'
+    print(numero, message)
 else:
     message = 'deve ser 0 ou 50'
     print(numero, message)
