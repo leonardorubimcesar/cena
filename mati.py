00ecdc752501f8a93abcae6959cbe434e6d9637e
leonardorubimcesar-byte <leonardorubimcesar@gmail.com>
2026-08-26 09:40:37 -0300

    versão 3 script aula 2

--- mati.py
+++ mati.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
-numero = 0
+numero = 49
 if numero < 0:
     message = 'negativo'
     print(message)
-elif numero > 0:
-    message = 'positivo'
+elif numero > 0 and numero < 50:
+    message = 'positivo e menor que 50'
+    print(message)
+elif numero > 0 and numero > 50:
+    message = 'positivo e maior que 50'
     print(message)
 else:
-    message = 'deve ser 0'
+    message = 'deve ser 0 ou 50'
     print(numero, message)
 
 
