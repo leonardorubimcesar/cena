48b372ec030bcde9ae87cafc6bd1ef6bfb29d702
leonardorubimcesar-byte <leonardorubimcesar@gmail.com>
2026-08-26 09:25:00 -0300

    outro script continuação pyhton2

---
+++ mati.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+numero = -32
+if numero < 0:
+    message = 'negativo'
+    print(message)
+elif numero > 0:
+    message = 'positivo'
+    print(message)
+else:
+    message = 'deve ser 0'
+    print(numero, message)
+
+
