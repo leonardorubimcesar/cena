efd36259848356d1a640ba3bef40b9a68916b84e
leonardorubimcesar-byte <leonardorubimcesar@gmail.com>
2026-08-28 11:30:02 -0300

    versão final

---
+++ dna.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+dna = 'ATGCTGCATT'
+rna = dna.replace('T', 'U')
+print(rna)
---
+++ strings.py
@@ -0,0 +1,2 @@
+#aula 3 python strings primeiro script
+#1 Sequências em Python são coleções ordenadas de dados com informação posicionadas
